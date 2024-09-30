d41618178e6966740936a86b11c30c40f68581cc
Yash <yashpatelv21@gmail.com>
2024-09-30 22:28:43 +0530

    date: 30/09/24

--- lib/main.dart
+++ lib/main.dart
@@ -275,3 +275,107 @@ class GenericScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+// class MoreScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         title: Row(
+//         children: [
+//     SizedBox(width: 8),
+//     Text(
+//     'VLC',
+//     style: TextStyle(
+//     color: Colors.black,
+//     fontSize: 24,
+//     fontWeight: FontWeight.bold,
+//     ),
+//     ),
+//     ],
+//     ),
+//     actions: [
+//     IconButton(
+//     icon: Icon(Icons.settings, color: Colors.black),
+//     onPressed: () {
+//     // TODO: Implement settings functionality
+//     },
+//     ),
+//     IconButton(
+//     icon: Icon(Icons.info_outline, color: Colors.black),
+//     onPressed: () {
+//     // TODO: Implement about functionality
+//     },
+//     ),
+//     IconButton(
+//     icon: Icon(Icons.more_vert, color: Colors.black),
+//     onPressed: () {
+//     // TODO: Implement more options functionality
+//     },
+//     ),
+//     ],
+//     elevation: 0,
+//     ),
+//     body: Padding(
+//     padding: const EdgeInsets.all(16.0),
+//     child: Column(
+//     crossAxisAlignment: CrossAxisAlignment.start,
+//     children: [
+//     // Streams Section
+//     Text(
+//     'Streams',
+//     style: TextStyle(
+//     color: Colors.orange,
+//     fontSize: 18,
+//     fontWeight: FontWeight.bold,
+//     ),
+//     ),
+//     SizedBox(height: 10),
+//     Container(
+//     height: 100,
+//     width: double.infinity,
+//     decoration: BoxDecoration(
+//     border: Border.all(color: Colors.grey),
+//     borderRadius: BorderRadius.circular(10),
+//     ),
+//     child: Center(
+//     child: Column(
+//     mainAxisAlignment: MainAxisAlignment.center,
+//     children: [
+//     Icon(Icons.add, size: 40, color: Colors.orange),
+//     Text('New stream', style: TextStyle(fontSize: 16)),
+//     ],
+//     ),
+//     ),
+//     ),
+//     SizedBox(height: 20),
+//
+//     // History Section
+//     Row(
+//     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//     children: [
+//     Text(
+//     'History',
+//     style: TextStyle(
+//     color: Colors.orange,
+//     fontSize: 18,
+//     fontWeight: FontWeight.bold,
+//     ),
+//     ),
+//     IconButton(
+//     icon: Icon(Icons.arrow_forward),
+//     onPressed: () {
+//     // TODO: Navigate to History screen
+//     },
+//     ),
+//     ],
+//     ),
+//     ],
+//     ),
+//     ),
+//     );
+//   }
+// }
